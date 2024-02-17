c2b88244b1a10ef4ba0ddc1e65f87acc36a6bdf7
Muneet786 <kumarmuneet440@gmail.com>
2024-02-17 20:10:17 +0500

    DOne

---
+++ Exercises with TypeScript and Node.js/Famous Quote/Quote.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log(`-Mother Teresa once said, Spread love everywhere you go. Let no one ever come to you without leaving happier.`);
---
+++ Exercises with TypeScript and Node.js/Famous Quote/Quote.ts
@@ -0,0 +1,1 @@
+console.log(`-Mother Teresa once said, Spread love everywhere you go. Let no one ever come to you without leaving happier.`);
