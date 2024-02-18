d2fead757fc8ed3d2aa6ed64c0082ee0040ceb12
Muneet786 <kumarmuneet440@gmail.com>
2024-02-18 17:36:25 +0500

    Done

---
+++ Exercises with TypeScript and Node.js/Famous Quote 2/app.ts
@@ -0,0 +1,5 @@
+let personName :string = 'Albert Einstein';
+
+let message :string = `"A person who never made a mistake never tried anything new."`;
+
+console.log(`${personName} once said , ${message}`);
