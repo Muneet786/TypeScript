b2f17e5e1583fca5d6fc697e70038c6c1ac1cdd5
Muneet786 <kumarmuneet440@gmail.com>
2024-02-18 20:23:03 +0500

    Done

---
+++ Exercises with TypeScript and Node.js/Stripping Names/app.js
@@ -0,0 +1,4 @@
+var person = "    \n\tAslam\t\n ";
+console.log(person);
+var without_Whitespace = person.trim();
+console.log(without_Whitespace);
---
+++ Exercises with TypeScript and Node.js/Stripping Names/app.ts
@@ -0,0 +1,7 @@
+const person : string = "    \n\tAslam\t\n ";
+
+console.log(person);
+
+const without_Whitespace : string = person.trim();
+
+console.log(without_Whitespace);
